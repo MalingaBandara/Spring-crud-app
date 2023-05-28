8a2bf84d4fd3a425f162c9d1756d2d7749f21131
Malinga Bandara <malinga1021@gmail.com>
2023-05-28 23:55:29 +0530

    Display all customers in view page as a table.
    - Hibernate
    - Spring
    -MySql

--- src/main/java/com/bitlord/app/controller/CustomerController.java
+++ src/main/java/com/bitlord/app/controller/CustomerController.java
@@ -1,17 +1,29 @@
 package com.bitlord.app.controller;
 
 
+import com.bitlord.app.dao.CustomerDao;
+import com.bitlord.app.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 
 @Controller
 @RequestMapping("/customers" )
 public class CustomerController {
 
+    @Autowired
+    private CustomerDao customerDao;
+
     @RequestMapping( "/all" )
     public String allCustomers(Model model){
+
+        List<Customer> list = customerDao.findAllCustomers();
+        model.addAttribute( "customers", list );
+
         return "all-customers";
     }
 
